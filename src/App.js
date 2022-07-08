3a34aec2b18a82e4e020244876acd5fe525ac3bd
Nicolas.Hintz <nico.hintz@hotmail.com>
2022-07-07 22:20:42 -0300

    componente con prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Navbar from './components/Navbar';
 import './App.css';
-
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   const title = "Mi Tienda"
 
   return (
-
-    <Navbar titulo = {title}/>
-    
-    );
+    <>
+      <Navbar titulo = {title}/>
+      <ItemListContainer greeting = {"New arrivals"} />
+    </>
+  );
 }
 
-      export default App;
+export default App;
